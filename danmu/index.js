ae50090e4fe0439eab856f321b4ad85463964330
zhangyu <grewer@grewer.cn>
2018-02-07 16:25:01 +0800

    init

--- danmu/index.js
+++ danmu/index.js
@@ -1,7 +1,73 @@
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
-var W = canvas.width = window.innerWidth;
+var W = window.innerWidth;
 var H = window.innerHeight;
-canvas.height = H * .6;
-var store = [];
+var pixelRatio = window.devicePixelRatio || 1;
+var backingStoreRatio = ctx.webkitBackingStorePixelRatio ||
+    ctx.mozBackingStorePixelRatio ||
+    ctx.msBackingStorePixelRatio ||
+    ctx.oBackingStorePixelRatio ||
+    ctx.backingStorePixelRatio || 1;
+var ratio = pixelRatio / backingStoreRatio;
+canvas.width = W * ratio;
+canvas.height = H * .6 * ratio;
+canvas.style.width = W + 'px';
+canvas.style.height = H * .6 + 'px';
+ctx.scale(ratio, ratio);
+// ctx.measureText(data).width
+var store = [
+    {
+        data: '测试,数量',
+        color: '#000',
+        location: W,
+        speed: 15,
+        dataWidth: 85.27999877929688,
+        level: 0
+    },
+    {
+        data: '测试,数量2',
+        color: '#000',
+        location: W,
+        speed: 10,
+        dataWidth: 97.27999877929688,
+        level: 7
+    }
+];
+ctx.font = '20px  宋体'; //根据高度分化出层次
+ctx.textBaseline = 'top'; //文字 base
+var maxLevel = H / 20 / 3 | 0;
+//7.3999
+console.log(maxLevel);
+var render = function () {
+    ctx.clearRect(0, 0, W, H);
+    var i = store.length;
+    while (i--) {
+        var dm = store[i];
+        ctx.fillStyle = dm.color;
+        if (!dm.level) {
+            //todo
+        }
+        ctx.fillText(dm.data, dm.location, dm.level * 30);
+        dm.location -= dm.speed;
+        if (dm.location + dm.dataWidth < 0) {
+            store.splice(i, 1);
+        }
+    }
+};
+var startTime = 0;
+var animation = function (timeStamp) {
+    if (timeStamp === void 0) { timeStamp = 0; }
+    if (timeStamp - startTime > 50) {
+        render();
+        startTime = timeStamp;
+    }
+    window.requestAnimationFrame(animation);
+};
+animation();
+var send = document.getElementById('send');
+var msg = document.getElementById('msg');
+send.onclick = function () {
+    if (!msg.value)
+        return;
+};
 //# sourceMappingURL=index.js.map
--- danmu/index.ts
+++ danmu/index.ts
@@ -1,10 +1,94 @@
 const canvas = <HTMLCanvasElement>document.getElementById('canvas');
-let ctx: any = canvas.getContext('2d');
-const W: number  = canvas.width=window.innerWidth;
-const H:number = window.innerHeight;
-canvas.height = H*.6;
+const ctx: any = canvas.getContext('2d');
+const W: number = window.innerWidth;
+const H: number = window.innerHeight;
 
-interface dataType{
-    data:string
+const pixelRatio = window.devicePixelRatio || 1;
+const backingStoreRatio = ctx.webkitBackingStorePixelRatio ||
+    ctx.mozBackingStorePixelRatio ||
+    ctx.msBackingStorePixelRatio ||
+    ctx.oBackingStorePixelRatio ||
+    ctx.backingStorePixelRatio || 1;
+const ratio = pixelRatio / backingStoreRatio;
+
+canvas.width = W * ratio;
+canvas.height = H * .6 * ratio;
+canvas.style.width = W + 'px';
+canvas.style.height = H * .6 + 'px';
+ctx.scale(ratio, ratio);
+
+
+interface dataType {
+    data: string
+    color: string
+    location: number
+    speed: number //根据 data 长度变化
+    dataWidth: number
+    level:number
 }
-let store:dataType[] = [];
+
+// ctx.measureText(data).width
+let store: dataType[] = [
+    {
+        data: '测试,数量',
+        color: '#000',
+        location: W,
+        speed: 15,
+        dataWidth: 85.27999877929688,
+        level:0
+
+    },
+    {
+        data: '测试,数量2',
+        color: '#000',
+        location: W,
+        speed: 10,
+        dataWidth: 97.27999877929688,
+        level:7
+    }
+];
+ctx.font = '20px  宋体';//根据高度分化出层次
+ctx.textBaseline = 'top';//文字 base
+
+const maxLevel:number = H/20/3 |0;
+//7.3999
+console.log(maxLevel);
+
+const render = (): void => {
+    ctx.clearRect(0, 0, W, H);
+    let i = store.length;
+    while (i--) {
+        let dm = store[i];
+        ctx.fillStyle = dm.color;
+        if(!dm.level){
+                //todo
+        }
+        ctx.fillText(dm.data, dm.location, dm.level*30);
+        dm.location -= dm.speed;
+        if (dm.location + dm.dataWidth < 0) {
+            store.splice(i, 1);
+        }
+    }
+};
+
+
+let startTime:number = 0;
+const animation = (timeStamp: number = 0):void => {
+    if (timeStamp - startTime > 50) {
+        render();
+        startTime = timeStamp;
+    }
+    window.requestAnimationFrame(animation);
+};
+
+animation();
+
+
+const send:any = document.getElementById('send');
+const msg:any = document.getElementById('msg');
+
+send.onclick = ():void =>{
+    if(!msg.value) return;
+
+};
+
