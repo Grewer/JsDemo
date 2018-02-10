9b18b136f2730a515f5450fef1b54d2ae2927642
zhangyu <grewer@grewer.cn>
2018-02-10 18:40:25 +0800

    横坐标差值计算

--- colorPicker/index.js
+++ colorPicker/index.js
@@ -13,13 +13,37 @@ pick.addEventListener('mousedown', function (ev) {
     console.log(ev);
     console.log(ev.offsetX, ev.offsetY);
     if (target.className === 'black') {
-        // console.log(ev);
+        // const currentColorArea = colorElement.style.backgroundColor;
+        var _a = getRGB(colorElement.style.backgroundColor), r = _a.r, g = _a.g, b = _a.b;
+        //右上 RGB;
+        console.log(r, g, b);
         var x = ev.offsetX, y = ev.offsetY;
-        var color = { r: 255, g: 255, b: 255 }, r = color.r, g = color.g, b = color.b;
+        var color = { r: 255, g: 255, b: 255 };
+        // {r,g,b} = color;
+        //左上角白色 255,255,255;
+        var difference = {
+            r: 255 - r,
+            g: 255 - g,
+            b: 255 - b
+        };
+        var rankX = x / colorWidth;
+        rankChange(difference, rankX);
+        console.log(difference);
+        var rankY = y / colorHeight;
         // 先根据x改变
         // console.log((1-x/colorWidth) * 255|0) //r
         // console.log((1-x/colorWidth) * 127.5|0) //rb
-        console.log((1 - y / colorHeight) * 255 | 0); //r,g,b
+        // console.log((1-y/colorHeight)*255 |0)//r,g,b
     }
 }, false);
+var rankChange = function (diff, rank) {
+    for (var i in diff) {
+        diff[i] = (rank * diff[i]) | 0;
+    }
+    return diff;
+};
+var getRGB = function (rgbString) {
+    var array = rgbString.split(',');
+    return { r: Number(array[0].split('(')[1]), g: Number(array[1]), b: Number(array[2].slice(0, -1)) };
+};
 //# sourceMappingURL=index.js.map
--- colorPicker/index.ts
+++ colorPicker/index.ts
@@ -16,18 +16,53 @@ pick.addEventListener('mousedown',(ev)=> {
     console.log(ev);
     console.log(ev.offsetX,ev.offsetY)
     if(target.className === 'black'){
-        // console.log(ev);
+        // const currentColorArea = colorElement.style.backgroundColor;
+        let {r,g,b} = getRGB(colorElement.style.backgroundColor);
+        //右上 RGB;
+        console.log(r,g,b);
         const x = ev.offsetX,y = ev.offsetY;
-        let color = {r:255,g:255,b:255},
-            {r,g,b} = color;
+        let color = {r:255,g:255,b:255};
+            // {r,g,b} = color;
+        //左上角白色 255,255,255;
+        const difference = {
+            r:255-r,
+            g:255-g,
+            b:255-b
+        };
+
+        const rankX = x/colorWidth;
+
+        rankChange(difference,rankX);
+        console.log(difference);
+
+
+        const rankY = y/colorHeight;
+
         // 先根据x改变
         // console.log((1-x/colorWidth) * 255|0) //r
         // console.log((1-x/colorWidth) * 127.5|0) //rb
 
 
-        console.log((1-y/colorHeight)*255 |0)//r,g,b
+        // console.log((1-y/colorHeight)*255 |0)//r,g,b
 
     }
 
 
 },false);
+interface rgb{
+    r:number
+    g:number
+    b:number
+}
+const rankChange = (diff:rgb,rank:number):rgb =>{
+    for(let i in diff){
+        diff[i] = (rank * diff[i]) |0;
+    }
+    return diff;
+};
+
+
+const getRGB = (rgbString:string):rgb =>{
+    let array:string[] = rgbString.split(',');
+    return {r:Number(array[0].split('(')[1]),g:Number(array[1]),b:Number(array[2].slice(0,-1))};
+};
