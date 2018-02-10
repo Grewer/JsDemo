ae266770dfa611648f6f7faddb68123329c90b6d
zhangyu <grewer@grewer.cn>
2018-02-10 23:03:05 +0800

    颜色改变

--- colorPicker/index.js
+++ colorPicker/index.js
@@ -1,49 +1,82 @@
 var pick = document.getElementById('pickBox');
 var colorElement = pick.querySelector('.color');
+var chooseColor = document.getElementById('chooseColor');
+var colorPoint = pick.querySelector('.point');
 //color长宽
 var colorWidth = colorElement.clientWidth;
 var colorHeight = colorElement.clientHeight;
 console.log(colorWidth, colorHeight);
+var isMoveColor = false;
+var changeColor = function (ev) {
+    var _a = getRGB(colorElement.style.backgroundColor), r = _a.r, g = _a.g, b = _a.b;
+    //右上 RGB;
+    var x = ev.offsetX, y = ev.offsetY;
+    colorPoint.style.left = x + 'px';
+    colorPoint.style.top = y + 'px';
+    var difference = {
+        r: 255 - r,
+        g: 255 - g,
+        b: 255 - b
+    };
+    var rankX = x / colorWidth;
+    rankChange(difference, rankX);
+    var result = {
+        r: 255 - difference.r,
+        g: 255 - difference.g,
+        b: 255 - difference.b
+    };
+    var rankY = y / colorHeight;
+    rankChange(result, 1 - rankY);
+    chooseColor.style.backgroundColor = objToRGB(result);
+};
+var rankChange = function (diff, rank) {
+    for (var i in diff) {
+        diff[i] = (rank * diff[i]) | 0;
+    }
+};
+var objToRGB = function (obj) {
+    return "rgb(" + obj.r + "," + obj.g + "," + obj.g + ")"; //后续加入透明度;
+};
+var getRGB = function (rgbString) {
+    var array = rgbString.split(',');
+    return { r: Number(array[0].split('(')[1]), g: Number(array[1]), b: Number(array[2].slice(0, -1)) };
+};
 pick.addEventListener('mousedown', function (ev) {
     var target = ev.target;
-    console.log(target);
     if (target.className === 'p') {
         console.log('移动坐标');
     }
     console.log(ev);
     console.log(ev.offsetX, ev.offsetY);
     if (target.className === 'black') {
-        // const currentColorArea = colorElement.style.backgroundColor;
-        var _a = getRGB(colorElement.style.backgroundColor), r = _a.r, g = _a.g, b = _a.b;
-        //右上 RGB;
-        console.log(r, g, b);
-        var x = ev.offsetX, y = ev.offsetY;
-        var color = { r: 255, g: 255, b: 255 };
-        // {r,g,b} = color;
-        //左上角白色 255,255,255;
-        var difference = {
-            r: 255 - r,
-            g: 255 - g,
-            b: 255 - b
-        };
-        var rankX = x / colorWidth;
-        rankChange(difference, rankX);
-        console.log(difference);
-        var rankY = y / colorHeight;
-        // 先根据x改变
-        // console.log((1-x/colorWidth) * 255|0) //r
-        // console.log((1-x/colorWidth) * 127.5|0) //rb
-        // console.log((1-y/colorHeight)*255 |0)//r,g,b
+        isMoveColor = true;
+        changeColor(ev);
+        return false;
     }
 }, false);
-var rankChange = function (diff, rank) {
-    for (var i in diff) {
-        diff[i] = (rank * diff[i]) | 0;
+document.addEventListener('mousemove', function (ev) {
+    if (isMoveColor === true) {
+        var target = ev.target;
+        if (target.className !== 'p' && target.className !== 'point') {
+            var x = ev.offsetX, y = ev.offsetY;
+            switch (true) {
+                case x < 0:
+                    x = 0;
+                case y < 0:
+                    y = 0;
+                case x > colorWidth:
+                    x = colorWidth;
+                case y > colorHeight:
+                    y = colorHeight;
+            }
+            changeColor(ev); //TODO 在选择颜色区域外有错误情况
+            colorPoint.style.left = x + 'px';
+            colorPoint.style.top = y + 'px';
+            return false;
+        }
     }
-    return diff;
-};
-var getRGB = function (rgbString) {
-    var array = rgbString.split(',');
-    return { r: Number(array[0].split('(')[1]), g: Number(array[1]), b: Number(array[2].slice(0, -1)) };
-};
+}, false);
+document.addEventListener('mouseup', function () {
+    isMoveColor = false;
+}, false);
 //# sourceMappingURL=index.js.map
--- colorPicker/index.ts
+++ colorPicker/index.ts
@@ -1,68 +1,99 @@
 const pick = <HTMLElement>document.getElementById('pickBox');
 const colorElement = <HTMLElement>pick.querySelector('.color');
-
+const chooseColor = <HTMLElement>document.getElementById('chooseColor');
+const colorPoint = <HTMLElement>pick.querySelector('.point');
 //color长宽
 const colorWidth = colorElement.clientWidth;
 const colorHeight = colorElement.clientHeight;
-console.log(colorWidth,colorHeight);
+console.log(colorWidth, colorHeight);
 
 
-pick.addEventListener('mousedown',(ev)=> {
+let isMoveColor: boolean = false;
+
+const changeColor = (ev):void =>{
+    let {r, g, b} = getRGB(colorElement.style.backgroundColor);
+    //右上 RGB;
+    const x = ev.offsetX, y = ev.offsetY;
+    colorPoint.style.left = x + 'px';
+    colorPoint.style.top = y + 'px';
+    const difference = {
+        r: 255 - r,
+        g: 255 - g,
+        b: 255 - b
+    };
+    const rankX = x / colorWidth;
+    rankChange(difference, rankX);
+    const result = {
+        r: 255 - difference.r,
+        g: 255 - difference.g,
+        b: 255 - difference.b
+    };
+    const rankY = y / colorHeight;
+
+    rankChange(result, 1 - rankY);
+    chooseColor.style.backgroundColor = objToRGB(result);
+};
+
+interface rgb {
+    r: number
+    g: number
+    b: number
+}
+
+const rankChange = (diff: rgb, rank: number): void => {
+    for (let i in diff) {
+        diff[i] = (rank * diff[i]) | 0;
+    }
+};
+
+const objToRGB = (obj: rgb): string => {
+    return `rgb(${obj.r},${obj.g},${obj.g})`;//后续加入透明度;
+};
+
+const getRGB = (rgbString: string): rgb => {
+    let array: string[] = rgbString.split(',');
+    return {r: Number(array[0].split('(')[1]), g: Number(array[1]), b: Number(array[2].slice(0, -1))};
+};
+
+
+pick.addEventListener('mousedown', (ev) => {
     const target = <HTMLElement>ev.target;
-    console.log(target);
-    if(target.className === 'p'){
+    if (target.className === 'p') {
         console.log('移动坐标');
     }
     console.log(ev);
-    console.log(ev.offsetX,ev.offsetY)
-    if(target.className === 'black'){
-        // const currentColorArea = colorElement.style.backgroundColor;
-        let {r,g,b} = getRGB(colorElement.style.backgroundColor);
-        //右上 RGB;
-        console.log(r,g,b);
-        const x = ev.offsetX,y = ev.offsetY;
-        let color = {r:255,g:255,b:255};
-            // {r,g,b} = color;
-        //左上角白色 255,255,255;
-        const difference = {
-            r:255-r,
-            g:255-g,
-            b:255-b
-        };
-
-        const rankX = x/colorWidth;
-
-        rankChange(difference,rankX);
-        console.log(difference);
-
-
-        const rankY = y/colorHeight;
-
-        // 先根据x改变
-        // console.log((1-x/colorWidth) * 255|0) //r
-        // console.log((1-x/colorWidth) * 127.5|0) //rb
-
-
-        // console.log((1-y/colorHeight)*255 |0)//r,g,b
-
+    console.log(ev.offsetX, ev.offsetY);
+    if (target.className === 'black') {
+        isMoveColor = true;
+        changeColor(ev);
+        return false;
     }
 
+}, false);
 
-},false);
-interface rgb{
-    r:number
-    g:number
-    b:number
-}
-const rankChange = (diff:rgb,rank:number):rgb =>{
-    for(let i in diff){
-        diff[i] = (rank * diff[i]) |0;
+document.addEventListener('mousemove', (ev) => {
+    if (isMoveColor === true) {
+        const target = <HTMLElement>ev.target;
+        if(target.className !== 'p' && target.className !== 'point' ){
+            let x = ev.offsetX, y = ev.offsetY;
+            switch (true) {
+                case x < 0:
+                    x = 0;
+                case y < 0:
+                    y = 0;
+                case x > colorWidth:
+                    x = colorWidth;
+                case y > colorHeight:
+                    y = colorHeight;
+            }
+            changeColor(ev);//TODO 在选择颜色区域外有错误情况
+            colorPoint.style.left = x + 'px';
+            colorPoint.style.top = y + 'px';
+            return false;
+        }
     }
-    return diff;
-};
+}, false);
 
-
-const getRGB = (rgbString:string):rgb =>{
-    let array:string[] = rgbString.split(',');
-    return {r:Number(array[0].split('(')[1]),g:Number(array[1]),b:Number(array[2].slice(0,-1))};
-};
+document.addEventListener('mouseup', () => {
+    isMoveColor = false;
+}, false);
