11f033002e9bc4f2eed001e0d9834ee1ffc6fe5e
zhangyu <grewer@grewer.cn>
2018-02-18 22:02:52 +0800

    加入测试按钮

--- danmu/index.js
+++ danmu/index.js
@@ -2,6 +2,7 @@ var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 var W = window.innerWidth;
 var H = window.innerHeight;
+var add10 = document.getElementById('random10');
 var pixelRatio = window.devicePixelRatio || 1;
 var backingStoreRatio = ctx.webkitBackingStorePixelRatio ||
     ctx.mozBackingStorePixelRatio ||
@@ -19,12 +20,17 @@ var store = [
     { data: '测试1号', color: '#ddd', location: W, speed: 15, dataWidth: 68.01998901367188 },
     { data: '测试1号233', color: '#333', location: W, speed: 15, dataWidth: 104.01998901367188 },
     { data: '测试1号23323', color: '#000', location: W, speed: 15, dataWidth: 128.01998901367188 },
-    { data: '测试1号2332334444', color: '#999', location: W, speed: 15, dataWidth: 188.01998901367188 },
-    { data: '测试1号2332334444', color: '#ddb974', location: W, speed: 15, dataWidth: 188.01998901367188 },
-    { data: '测试1号2332334444', color: '#D83424', location: W, speed: 15, dataWidth: 188.01998901367188 },
+    { data: '测试1号23323323jd', color: '#999', location: W, speed: 18, dataWidth: 188.01998901367188 },
+    { data: '测试1号2332334444', color: '#ddb974', location: W, speed: 17, dataWidth: 188.01998901367188 },
+    { data: '测试1号2332334496', color: '#D83424', location: W, speed: 16, dataWidth: 188.01998901367188 },
     { data: '测试1号2332334444', color: '#6bcedd', location: W, speed: 15, dataWidth: 188.01998901367188 },
-    { data: '测试1号2332334444', color: '#dd4ba6', location: W, speed: 15, dataWidth: 188.01998901367188 },
-    { data: '测试1号2332334444', color: '#82dd75', location: W, speed: 15, dataWidth: 188.01998901367188 }
+    { data: '测试1号2332334497', color: '#dd4ba6', location: W, speed: 18, dataWidth: 188.01998901367188 },
+    { data: '测试1号2332334444', color: '#82dd75', location: W, speed: 15, dataWidth: 188.01998901367188 },
+    { data: '测试1号2332334423', color: '#82dd75', location: W, speed: 15, dataWidth: 188.01998901367188 },
+    { data: '测试1号2332334444', color: '#6bcedd', location: W, speed: 10, dataWidth: 188.01998901367188 },
+    { data: '测试1号2332334498', color: '#dd4ba6', location: W, speed: 15, dataWidth: 188.01998901367188 },
+    { data: '测试1号2332334444', color: '#82dd75', location: W, speed: 10, dataWidth: 188.01998901367188 },
+    { data: '测试1号2332334423', color: '#82dd75', location: W, speed: 15, dataWidth: 188.01998901367188 }
 ];
 ctx.font = '20px  宋体'; //根据高度分化出层次
 ctx.textBaseline = 'top'; //文字 base
@@ -52,11 +58,13 @@ var addLevel = function (dm) {
     while (i--) {
         var sdm = store[i];
         if (sdm.location + sdm.dataWidth > W) {
-            test[sdm.level] = sdm.location + sdm.dataWidth;
+            // test[sdm.level] = sdm.location + sdm.dataWidth;
+            test[sdm.level] = { location: sdm.location, speed: sdm.speed, short: sdm.location + sdm.dataWidth };
         }
     }
     var short = W;
     var level = 0;
+    var speed = 10;
     for (var i_1 = 0; i_1 < maxLevel; i_1++) {
         if (!test[i_1]) {
             dm.level = i_1;
@@ -64,20 +72,30 @@ var addLevel = function (dm) {
         }
         // console.log(test);
         if (i_1 === 0) {
-            short = test[0];
+            console.log(short);
+            short = test[0].location;
         }
-        if (test[i_1] < short) {
+        if (test[i_1].location < W) {
             // console.log('test',test[i],i,short)
-            short = test[i_1];
+            short = test[i_1].short;
+            speed = test[i_1].speed;
             level = i_1;
         }
         if (i_1 === maxLevel - 1) {
             // console.log(short,level);
-            dm.location = short + 50; //+50
+            console.log(dm);
+            console.log(speed, short);
+            if (dm.speed > speed) {
+                dm.location = short + (short / 10) * (dm.speed - speed);
+            }
+            else {
+                dm.location = short + 50;
+            }
             dm.level = level;
         }
     }
 };
+// TODO 获取 store 中 1-7 列中 每一列中最长的 对比其他列最短的那一条;  在那一条后加入;
 var startTime = 0;
 var animation = function (timeStamp) {
     if (timeStamp === void 0) { timeStamp = 0; }
@@ -94,8 +112,11 @@ send.onclick = function () {
     if (!msg.value)
         return;
     var width = ctx.measureText(msg.value).width;
-    // const speed = (W / width) < 10 ? 10 : W / width
-    var speed = 15; //暂为15
+    var speed = W / width;
+    if (speed > 20)
+        speed = 20;
+    if (speed < 10)
+        speed = 10;
     store.push({
         data: msg.value,
         color: '#000',
@@ -104,5 +125,33 @@ send.onclick = function () {
         dataWidth: width,
     });
 };
-//TODO 颜色选择器
+var resources = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var getOne = function () {
+    var length = Math.random() * 10 | 2 + 2;
+    var result = '';
+    for (var i = 0; i < length; i++) {
+        result += resources[Math.random() * resources.length | 0];
+    }
+    return result;
+};
+add10.onclick = function () {
+    for (var i = 0; i < 10; i++) {
+        var word = getOne();
+        var width = ctx.measureText(word).width;
+        var speed = W / width;
+        if (speed > 20)
+            speed = 20;
+        if (speed < 10)
+            speed = 10;
+        store.push({
+            data: word,
+            color: '#000',
+            location: W,
+            speed: speed,
+            dataWidth: width,
+        });
+    }
+};
+//TODO 颜色选择器待加入
+// TODO 加入一个10速,靠近左边时,再加入速度20的, 会追上;
 //# sourceMappingURL=index.js.map
--- danmu/index.ts
+++ danmu/index.ts
@@ -3,6 +3,9 @@ const ctx: any = canvas.getContext('2d');
 const W: number = window.innerWidth;
 const H: number = window.innerHeight;
 
+const add10 = document.getElementById('random10');
+
+
 const pixelRatio = window.devicePixelRatio || 1;
 const backingStoreRatio = ctx.webkitBackingStorePixelRatio ||
     ctx.mozBackingStorePixelRatio ||
@@ -32,13 +35,17 @@ let store: dataType[] = [
     {data: '测试1号', color: '#ddd', location: W, speed: 15, dataWidth: 68.01998901367188},
     {data: '测试1号233', color: '#333', location: W, speed: 15, dataWidth: 104.01998901367188},
     {data: '测试1号23323', color: '#000', location: W, speed: 15, dataWidth: 128.01998901367188},
-    {data: '测试1号2332334444', color: '#999', location: W, speed: 15, dataWidth: 188.01998901367188},
-    {data: '测试1号2332334444', color: '#ddb974', location: W, speed: 15, dataWidth: 188.01998901367188},
-    {data: '测试1号2332334444', color: '#D83424', location: W, speed: 15, dataWidth: 188.01998901367188},
+    {data: '测试1号23323323jd', color: '#999', location: W, speed: 18, dataWidth: 188.01998901367188},
+    {data: '测试1号2332334444', color: '#ddb974', location: W, speed: 17, dataWidth: 188.01998901367188},
+    {data: '测试1号2332334496', color: '#D83424', location: W, speed: 16, dataWidth: 188.01998901367188},
     {data: '测试1号2332334444', color: '#6bcedd', location: W, speed: 15, dataWidth: 188.01998901367188},
-    {data: '测试1号2332334444', color: '#dd4ba6', location: W, speed: 15, dataWidth: 188.01998901367188},
-    {data: '测试1号2332334444', color: '#82dd75', location: W, speed: 15, dataWidth: 188.01998901367188}
-
+    {data: '测试1号2332334497', color: '#dd4ba6', location: W, speed: 18, dataWidth: 188.01998901367188},
+    {data: '测试1号2332334444', color: '#82dd75', location: W, speed: 15, dataWidth: 188.01998901367188},
+    {data: '测试1号2332334423', color: '#82dd75', location: W, speed: 15, dataWidth: 188.01998901367188},
+    {data: '测试1号2332334444', color: '#6bcedd', location: W, speed: 10, dataWidth: 188.01998901367188},
+    {data: '测试1号2332334498', color: '#dd4ba6', location: W, speed: 15, dataWidth: 188.01998901367188},
+    {data: '测试1号2332334444', color: '#82dd75', location: W, speed: 10, dataWidth: 188.01998901367188},
+    {data: '测试1号2332334423', color: '#82dd75', location: W, speed: 15, dataWidth: 188.01998901367188}
 ];
 ctx.font = '20px  宋体';//根据高度分化出层次
 ctx.textBaseline = 'top';//文字 base
@@ -63,17 +70,26 @@ const render = (): void => {
     }
 };
 
+interface testType {
+    location: number
+    speed: number
+    short:number
+}
+
 const addLevel = (dm): void => {
     let i: number = store.length;
-    let test: number[] = [];
+    let test: testType[] = [];
     while (i--) {
         let sdm = store[i];
         if (sdm.location + sdm.dataWidth > W) {
-            test[sdm.level] = sdm.location + sdm.dataWidth;
+            // test[sdm.level] = sdm.location + sdm.dataWidth;
+            test[sdm.level] = {location: sdm.location, speed: sdm.speed,short:sdm.location + sdm.dataWidth};
+
         }
     }
     let short: number = W;
     let level: number = 0;
+    let speed: number = 10;
     for (let i = 0; i < maxLevel; i++) {
         if (!test[i]) {
             dm.level = i;
@@ -81,20 +97,30 @@ const addLevel = (dm): void => {
         }
         // console.log(test);
         if (i === 0) {
-            short = test[0];
+            console.log(short);
+            short = test[0].location;
         }
-        if (test[i] < short) {
+        if (test[i].location < W) { //TODO 未触发的 bug
             // console.log('test',test[i],i,short)
-            short = test[i];
+            short = test[i].short;
+            speed = test[i].speed;
             level = i;
         }
         if (i === maxLevel - 1) {
             // console.log(short,level);
-            dm.location = short + 50;//+50
+            console.log(dm);
+            console.log(speed,short);
+
+            if (dm.speed > speed) {
+                dm.location = short + (short / 10) * (dm.speed - speed);
+            } else {
+                dm.location = short + 50;
+            }
             dm.level = level;
         }
     }
 };
+// TODO 获取 store 中 1-7 列中 每一列中最长的 对比其他列最短的那一条;  在那一条后加入;
 
 let startTime: number = 0;
 const animation = (timeStamp: number = 0): void => {
@@ -114,8 +140,9 @@ const msg: any = document.getElementById('msg');
 send.onclick = (): void => {
     if (!msg.value) return;
     const width = ctx.measureText(msg.value).width;
-    // const speed = (W / width) < 10 ? 10 : W / width
-    const speed = 15;//暂为15
+    let speed = W / width;
+    if (speed > 20) speed = 20;
+    if (speed < 10) speed = 10;
     store.push({
         data: msg.value,
         color: '#000',
@@ -124,4 +151,38 @@ send.onclick = (): void => {
         dataWidth: width,
     })
 };
-//TODO 颜色选择器
+
+
+const resources = [1,2,3,4,5,6,7,8,9,0,"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","X","Y","Z","a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+const getOne = ():string=>{
+  const length:number = Math.random()*10|2 +2;
+  let result = '';
+  for(let i=0;i<length;i++){
+      result += resources[Math.random()*resources.length|0];
+  }
+
+  return result;
+};
+
+add10.onclick = ():void =>{
+    for(let i=0;i<10;i++){
+        const word = getOne();
+        const width = ctx.measureText(word).width;
+        let speed = W / width;
+        if (speed > 20) speed = 20;
+        if (speed < 10) speed = 10;
+        store.push({
+            data: word,
+            color: '#000',
+            location: W,
+            speed,
+            dataWidth: width,
+        })
+    }
+};
+
+
+//TODO 颜色选择器待加入
+
+// TODO 加入一个10速,靠近左边时,再加入速度20的, 会追上;
