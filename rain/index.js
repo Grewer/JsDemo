c2a61b69ff190abcd820f85f2716215ee98eabe6
zhangyu <grewer@grewer.cn>
2018-03-04 23:51:52 +0800

    add base drops

--- rain/index.js
+++ rain/index.js
@@ -16,7 +16,7 @@ canvas.style.height = H + 'px';
 ctx.scale(ratio, ratio);
 var store = [
     {
-        posi: {
+        pos: {
             x: 200,
             y: 0
         },
@@ -24,25 +24,39 @@ var store = [
         drops: false
     }
 ];
-ctx.fillStyle = "#000";
+var dropsStore = [];
 ctx.strokeStyle = "#fff";
 ctx.moveTo(0, 0);
 var getParabolaFunc = function (_a, _b) {
     var px = _a.px, py = _a.py;
     var _c = _b.x, x = _c === void 0 ? 0 : _c, _d = _b.y, y = _d === void 0 ? 0 : _d;
     //顶点,某一点
-    var a = (y - py) / Math.pow(x - px, 2);
-    var b = 2 * -px * a;
-    var c = px * px * a + py;
+    //缓存,暂定
+    var a = (y - py) / Math.pow(x - px, 2), b = 2 * -px * a, c = px * px * a + py;
     console.log(a, b, c);
     return function (x) {
         return a * x * x + b * x + c;
     };
 };
+var addDrops = function (x) {
+    var i = Math.random() * 10 + 2 | 0;
+    // i=1;
+    while (i--) {
+        var offsetX = Math.random() * 50 - 25;
+        var speed = offsetX > 0 ? 2 : -2;
+        dropsStore.push({
+            pos: { x: x, y: H },
+            fn: getParabolaFunc({ px: offsetX + x, py: H - Math.random() * 50 }, { x: x, y: H }),
+            speed: speed
+        });
+    }
+    console.log(dropsStore);
+};
 var func;
 //
 var render = function () {
     // console.log('run');
+    ctx.fillStyle = "#000";
     ctx.fillRect(0, 0, W, H);
     // if (!func) {
     //     func = getParabolaFunc({px: 200, py: 200}, {x, y});
@@ -55,15 +69,38 @@ var render = function () {
     for (var i = 0, l = store.length; i < l; i++) {
         ctx.beginPath();
         var storeCase = store[i];
-        if (storeCase.posi.y > H) {
+        if (storeCase.pos.y > H) {
             store.splice(i, 1);
             continue;
         }
-        ctx.moveTo(storeCase.posi.x, storeCase.posi.y);
-        ctx.lineTo(storeCase.posi.x, storeCase.posi.y + storeCase.height);
-        storeCase.posi.y += 12;
+        ctx.moveTo(storeCase.pos.x, storeCase.pos.y);
+        ctx.lineTo(storeCase.pos.x, storeCase.pos.y + storeCase.height);
+        storeCase.pos.y += 12;
+        if (storeCase.pos.y + storeCase.height > H && !storeCase.drops) {
+            console.log('drops');
+            addDrops(storeCase.pos.x);
+            storeCase.drops = true;
+        }
     }
     ctx.stroke();
+    for (var i = 0, l = dropsStore.length; i < l; i++) {
+        ctx.beginPath();
+        var drop = dropsStore[i];
+        console.log(drop);
+        // if(drop.pos.y>H){
+        //     delete dropsStore[i];
+        //     continue;
+        // }
+        ctx.fillStyle = "#fff";
+        ctx.arc(drop.pos.x, drop.pos.y, 2, 0, 2 * Math.PI);
+        drop.pos.x += drop.speed;
+        drop.pos.y = drop.fn(drop.pos.x);
+        // if(drop.pos.y>H){
+        //     delete dropsStore[i];
+        //     continue;
+        // }
+    }
+    ctx.fill();
     // if (x > 400) {
     //     return;
     // }
--- rain/index.ts
+++ rain/index.ts
@@ -20,39 +20,77 @@ canvas.style.height = H + 'px';
 
 ctx.scale(ratio, ratio);
 
+interface coordinateType {
+    x: number
+    y: number
+}
 
-let store = [
+interface rainType {
+    pos: coordinateType
+    height: number
+    drops: boolean
+}
+
+let store: rainType[] = [
     {
-        posi:{
-            x:200,
-            y:0
+        pos: {
+            x: 200,
+            y: 0
         },//坐标
-        height:70,//长度
-        drops:false
+        height: 70,//长度
+        drops: false
     }
 ];
 
+interface dropsType {
+    pos: coordinateType
+    fn: Function
+    speed:number
+}
 
-ctx.fillStyle="#000";
-ctx.strokeStyle = "#fff"
+let dropsStore: dropsType[] = [];
+
+
+ctx.strokeStyle = "#fff";
 
 ctx.moveTo(0, 0);
-const getParabolaFunc = ({px, py}, {x = 0, y = 0}):Function => {
+const getParabolaFunc = ({px, py}, {x = 0, y = 0}): Function => {
     //顶点,某一点
-    const a = (y - py) / Math.pow(x - px, 2);
-    const b = 2 * -px * a;
-    const c = px * px * a + py;
+    //缓存,暂定
+    const a: number = (y - py) / Math.pow(x - px, 2),
+        b: number = 2 * -px * a,
+        c: number = px * px * a + py;
     console.log(a, b, c);
     return (x) => {
         return a * x * x + b * x + c;
     }
 };
 
+const addDrops = (x)=>{
+    let i = Math.random()*10+2|0;
+    // i=1;
+    while (i--){
+        const offsetX =  Math.random()*50-25;
+        const speed = offsetX >0 ? 2: -2;
+        dropsStore.push(
+            {
+                pos:{x,y:H},
+                fn:getParabolaFunc({px:offsetX+x,py:H-Math.random()*50},{x,y:H}),
+                speed
+            }
+        )
+    }
+    console.log(dropsStore)
+
+};
+
+
 let func;
 //
 const render = (): void => {
     // console.log('run');
-    ctx.fillRect(0,0,W,H);
+    ctx.fillStyle = "#000";
+    ctx.fillRect(0, 0, W, H);
 
     // if (!func) {
     //     func = getParabolaFunc({px: 200, py: 200}, {x, y});
@@ -63,20 +101,46 @@ const render = (): void => {
     // console.log(x, func(x));
     //
 
-    for(let i=0,l=store.length;i<l;i++){
+    for (let i = 0, l = store.length; i < l; i++) {
         ctx.beginPath();
-        let storeCase = store[i];
-        if(storeCase.posi.y>H){
-            store.splice(i,1);
+        const storeCase: rainType = store[i];
+        if (storeCase.pos.y > H) {
+            store.splice(i, 1);
             continue;
         }
-        ctx.moveTo(storeCase.posi.x,storeCase.posi.y);
-        ctx.lineTo(storeCase.posi.x,storeCase.posi.y+storeCase.height);
-        storeCase.posi.y+=12;
-
+        ctx.moveTo(storeCase.pos.x, storeCase.pos.y);
+        ctx.lineTo(storeCase.pos.x, storeCase.pos.y + storeCase.height);
+        storeCase.pos.y += 12;
+        if (storeCase.pos.y + storeCase.height > H && !storeCase.drops) {
+            console.log('drops')
+            addDrops(storeCase.pos.x);
+            storeCase.drops = true;
+        }
 
     }
     ctx.stroke();
+
+    for(let i=0,l=dropsStore.length;i<l;i++){
+        ctx.beginPath();
+
+        const drop = dropsStore[i];
+        console.log(drop);
+        // if(drop.pos.y>H){
+        //     delete dropsStore[i];
+        //     continue;
+        // }
+        ctx.fillStyle = "#fff";
+        ctx.arc(drop.pos.x,drop.pos.y,2,0,2*Math.PI);
+        drop.pos.x+=drop.speed;
+        drop.pos.y = drop.fn(drop.pos.x);
+        // if(drop.pos.y>H){
+        //     delete dropsStore[i];
+        //     continue;
+        // }
+    }
+    ctx.fill();
+
+
 
 
     // if (x > 400) {
