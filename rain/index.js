dfd5ba211f0319f7da0afc4b91792697d8cdef35
zhangyu <grewer@grewer.cn>
2018-03-05 17:06:01 +0800

    update

--- rain/index.js
+++ rain/index.js
@@ -1,110 +1,108 @@
-var canvas = document.getElementById('canvas');
-var ctx = canvas.getContext('2d');
-var W = window.innerWidth;
-var H = window.innerHeight;
-var pixelRatio = window.devicePixelRatio || 1;
-var backingStoreRatio = ctx.webkitBackingStorePixelRatio ||
-    ctx.mozBackingStorePixelRatio ||
-    ctx.msBackingStorePixelRatio ||
-    ctx.oBackingStorePixelRatio ||
-    ctx.backingStorePixelRatio || 1;
-var ratio = pixelRatio / backingStoreRatio;
-canvas.width = W * ratio;
-canvas.height = H * ratio;
-canvas.style.width = W + 'px';
-canvas.style.height = H + 'px';
-ctx.scale(ratio, ratio);
-var store = [
-    {
-        pos: {
-            x: 200,
-            y: 0
-        },
-        height: 70,
-        drops: false
-    }
-];
-var dropsStore = [];
-ctx.strokeStyle = "#fff";
-ctx.moveTo(0, 0);
-var getParabolaFunc = function (_a, _b) {
-    var px = _a.px, py = _a.py;
-    var _c = _b.x, x = _c === void 0 ? 0 : _c, _d = _b.y, y = _d === void 0 ? 0 : _d;
-    //顶点,某一点
-    //缓存,暂定
-    var a = (y - py) / Math.pow(x - px, 2), b = 2 * -px * a, c = px * px * a + py;
-    console.log(a, b, c);
-    return function (x) {
-        return a * x * x + b * x + c;
+(function () {
+    var canvas = document.getElementById('canvas');
+    var ctx = canvas.getContext('2d');
+    var W = window.innerWidth;
+    var H = window.innerHeight;
+    var pixelRatio = window.devicePixelRatio || 1;
+    var backingStoreRatio = ctx.webkitBackingStorePixelRatio ||
+        ctx.mozBackingStorePixelRatio ||
+        ctx.msBackingStorePixelRatio ||
+        ctx.oBackingStorePixelRatio ||
+        ctx.backingStorePixelRatio || 1;
+    var ratio = pixelRatio / backingStoreRatio;
+    canvas.width = W * ratio;
+    canvas.height = H * ratio;
+    canvas.style.width = W + 'px';
+    canvas.style.height = H + 'px';
+    ctx.scale(ratio, ratio);
+    var grad = ctx.createLinearGradient(0, 0, 0, H);
+    /* 指定几个颜色 */
+    grad.addColorStop(0, '#688896');
+    grad.addColorStop(1, '#423d38');
+    var store = [];
+    var dropsStore = [];
+    var dropsCache = {};
+    ctx.strokeStyle = "#6EACEC";
+    ctx.moveTo(0, 0);
+    var getParabolaFunc = function (_a, _b) {
+        var px = _a.px, py = _a.py;
+        var _c = _b.x, x = _c === void 0 ? 0 : _c, _d = _b.y, y = _d === void 0 ? 0 : _d;
+        var isExist = dropsCache[px + "-" + py + "-" + x + "-" + y];
+        if (isExist) {
+            return isExist;
+        }
+        var a = (y - py) / Math.pow(x - px, 2), b = 2 * -px * a, c = px * px * a + py;
+        dropsCache[px + "-" + py + "-" + x + "-" + y] = function (x) {
+            return a * x * x + b * x + c;
+        };
+        return dropsCache[px + "-" + py + "-" + x + "-" + y];
     };
-};
-var addDrops = function (x) {
-    var i = Math.random() * 10 + 2 | 0;
-    // i=1;
-    while (i--) {
-        var offsetX = Math.random() * 50 - 25;
-        var speed = offsetX > 0 ? 2 : -2;
-        dropsStore.push({
-            pos: { x: x, y: H },
-            fn: getParabolaFunc({ px: offsetX + x, py: H - Math.random() * 50 }, { x: x, y: H }),
-            speed: speed
-        });
-    }
-    console.log(dropsStore);
-};
-var func;
-//
-var render = function () {
-    // console.log('run');
-    ctx.fillStyle = "#000";
-    ctx.fillRect(0, 0, W, H);
-    // if (!func) {
-    //     func = getParabolaFunc({px: 200, py: 200}, {x, y});
-    // }
-    //
-    // ctx.lineTo(x += 1, func(x));
-    //
-    // console.log(x, func(x));
-    //
-    for (var i = 0, l = store.length; i < l; i++) {
-        ctx.beginPath();
-        var storeCase = store[i];
-        if (storeCase.pos.y > H) {
-            store.splice(i, 1);
-            continue;
+    var addDrops = function (x) {
+        var i = Math.random() * 10 + 2 | 0;
+        while (i--) {
+            var offsetX = Math.random() * 50 - 25;
+            var speed = offsetX > 0 ? 2 : -2;
+            dropsStore.push({
+                pos: { x: x, y: H },
+                fn: getParabolaFunc({ px: offsetX + x, py: H - Math.random() * 40 - 10 }, { x: x, y: H }),
+                speed: speed,
+                radius: Math.random() + 1
+            });
         }
-        ctx.moveTo(storeCase.pos.x, storeCase.pos.y);
-        ctx.lineTo(storeCase.pos.x, storeCase.pos.y + storeCase.height);
-        storeCase.pos.y += 12;
-        if (storeCase.pos.y + storeCase.height > H && !storeCase.drops) {
-            console.log('drops');
-            addDrops(storeCase.pos.x);
-            storeCase.drops = true;
+    };
+    var startTime = 0;
+    var render = function (timeStamp) {
+        if (timeStamp === void 0) { timeStamp = 0; }
+        ctx.fillStyle = grad;
+        ctx.fillRect(0, 0, W, H);
+        for (var i = 0, l = store.length; i < l; i++) {
+            var storeCase = store[i];
+            if (!storeCase)
+                continue;
+            ctx.beginPath();
+            if (storeCase.pos.y > H) {
+                //报错
+                store.splice(i, 1);
+                continue;
+            }
+            ctx.moveTo(storeCase.pos.x, storeCase.pos.y);
+            ctx.lineTo(storeCase.pos.x, storeCase.pos.y + storeCase.height);
+            storeCase.pos.y += 12;
+            if (storeCase.pos.y + storeCase.height > H && !storeCase.drops) {
+                addDrops(storeCase.pos.x);
+                storeCase.drops = true;
+            }
+            ctx.stroke();
         }
-    }
-    ctx.stroke();
-    for (var i = 0, l = dropsStore.length; i < l; i++) {
-        ctx.beginPath();
-        var drop = dropsStore[i];
-        console.log(drop);
-        // if(drop.pos.y>H){
-        //     delete dropsStore[i];
-        //     continue;
-        // }
-        ctx.fillStyle = "#fff";
-        ctx.arc(drop.pos.x, drop.pos.y, 2, 0, 2 * Math.PI);
-        drop.pos.x += drop.speed;
-        drop.pos.y = drop.fn(drop.pos.x);
-        // if(drop.pos.y>H){
-        //     delete dropsStore[i];
-        //     continue;
-        // }
-    }
-    ctx.fill();
-    // if (x > 400) {
-    //     return;
-    // }
-    window.requestAnimationFrame(render);
-};
-render();
+        for (var i = 0, l = dropsStore.length; i < l; i++) {
+            var drop = dropsStore[i];
+            if (!drop)
+                continue;
+            ctx.beginPath();
+            if (drop.pos.y > H) {
+                dropsStore.splice(i, 1);
+                continue;
+            }
+            ctx.fillStyle = "#6EACEC";
+            ctx.arc(drop.pos.x, drop.pos.y, drop.radius, 0, 2 * Math.PI);
+            ctx.fill();
+            drop.pos.x += drop.speed;
+            drop.pos.y = drop.fn(drop.pos.x);
+        }
+        if (timeStamp - startTime > 200) {
+            store.push({
+                pos: {
+                    x: Math.random() * W,
+                    y: 0
+                },
+                height: 50 * Math.random() + 20,
+                drops: false
+            });
+            startTime = timeStamp;
+        }
+        // console.log(timeStamp);
+        window.requestAnimationFrame(render);
+    };
+    render();
+})();
 //# sourceMappingURL=index.js.map
--- rain/index.ts
+++ rain/index.ts
@@ -1,152 +1,149 @@
-const canvas = <HTMLCanvasElement>document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
+(() => {
+        const canvas = <HTMLCanvasElement>document.getElementById('canvas');
+        const ctx = canvas.getContext('2d');
 
 
-const W: number = window.innerWidth;
-const H: number = window.innerHeight;
+        const W: number = window.innerWidth;
+        const H: number = window.innerHeight;
 
-const pixelRatio = window.devicePixelRatio || 1;
-const backingStoreRatio = ctx.webkitBackingStorePixelRatio ||
-    ctx.mozBackingStorePixelRatio ||
-    ctx.msBackingStorePixelRatio ||
-    ctx.oBackingStorePixelRatio ||
-    ctx.backingStorePixelRatio || 1;
-const ratio = pixelRatio / backingStoreRatio;
+        const pixelRatio = window.devicePixelRatio || 1;
+        const backingStoreRatio = ctx.webkitBackingStorePixelRatio ||
+            ctx.mozBackingStorePixelRatio ||
+            ctx.msBackingStorePixelRatio ||
+            ctx.oBackingStorePixelRatio ||
+            ctx.backingStorePixelRatio || 1;
+        const ratio = pixelRatio / backingStoreRatio;
 
-canvas.width = W * ratio;
-canvas.height = H * ratio;
-canvas.style.width = W + 'px';
-canvas.style.height = H + 'px';
+        canvas.width = W * ratio;
+        canvas.height = H * ratio;
+        canvas.style.width = W + 'px';
+        canvas.style.height = H + 'px';
 
-ctx.scale(ratio, ratio);
+        ctx.scale(ratio, ratio);
 
-interface coordinateType {
-    x: number
-    y: number
-}
-
-interface rainType {
-    pos: coordinateType
-    height: number
-    drops: boolean
-}
-
-let store: rainType[] = [
-    {
-        pos: {
-            x: 200,
-            y: 0
-        },//坐标
-        height: 70,//长度
-        drops: false
-    }
-];
-
-interface dropsType {
-    pos: coordinateType
-    fn: Function
-    speed:number
-}
-
-let dropsStore: dropsType[] = [];
+        const grad = ctx.createLinearGradient(0, 0, 0, H);
+        /* 指定几个颜色 */
+        grad.addColorStop(0, '#688896');
+        grad.addColorStop(1, '#423d38');
 
 
-ctx.strokeStyle = "#fff";
+        interface coordinateType {
+            x: number
+            y: number
+        }
 
-ctx.moveTo(0, 0);
-const getParabolaFunc = ({px, py}, {x = 0, y = 0}): Function => {
-    //顶点,某一点
-    //缓存,暂定
-    const a: number = (y - py) / Math.pow(x - px, 2),
-        b: number = 2 * -px * a,
-        c: number = px * px * a + py;
-    console.log(a, b, c);
-    return (x) => {
-        return a * x * x + b * x + c;
-    }
-};
+        interface rainType {
+            pos: coordinateType
+            height: number
+            drops: boolean
+        }
 
-const addDrops = (x)=>{
-    let i = Math.random()*10+2|0;
-    // i=1;
-    while (i--){
-        const offsetX =  Math.random()*50-25;
-        const speed = offsetX >0 ? 2: -2;
-        dropsStore.push(
-            {
-                pos:{x,y:H},
-                fn:getParabolaFunc({px:offsetX+x,py:H-Math.random()*50},{x,y:H}),
-                speed
+        let store: rainType[] = [];
+
+        interface dropsType {
+            pos: coordinateType
+            fn: Function
+            speed: number
+            radius: number
+        }
+
+        let dropsStore: dropsType[] = [];
+
+        let dropsCache = {};
+        ctx.strokeStyle = "#6EACEC";
+
+        ctx.moveTo(0, 0);
+        const getParabolaFunc = ({px, py}, {x = 0, y = 0}): Function => {
+            const isExist = dropsCache[`${px}-${py}-${x}-${y}`];
+            if (isExist) {
+                return isExist;
             }
-        )
+            const a: number = (y - py) / Math.pow(x - px, 2),
+                b: number = 2 * -px * a,
+                c: number = px * px * a + py;
+            dropsCache[`${px}-${py}-${x}-${y}`] = (x) => {
+                return a * x * x + b * x + c;
+            };
+            return dropsCache[`${px}-${py}-${x}-${y}`]
+        };
+
+        const addDrops = (x) => {
+            let i = Math.random() * 10 + 2 | 0;
+            while (i--) {
+                const offsetX = Math.random() * 50 - 25;
+                const speed = offsetX > 0 ? 2 : -2;
+                dropsStore.push(
+                    {
+                        pos: {x, y: H},
+                        fn: getParabolaFunc({px: offsetX + x, py: H - Math.random() * 40 - 10}, {x, y: H}),
+                        speed,
+                        radius: Math.random() + 1
+                    }
+                )
+            }
+
+        };
+
+        let startTime: number = 0;
+        const render = (timeStamp = 0): void => {
+            ctx.fillStyle = grad;
+            ctx.fillRect(0, 0, W, H);
+
+            for (let i = 0, l = store.length; i < l; i++) {
+                const storeCase: rainType = store[i];
+                if (!storeCase) continue;
+                ctx.beginPath();
+                if (storeCase.pos.y > H) {
+                    //报错
+                    store.splice(i, 1);
+                    continue;
+                }
+                ctx.moveTo(storeCase.pos.x, storeCase.pos.y);
+                ctx.lineTo(storeCase.pos.x, storeCase.pos.y + storeCase.height);
+                storeCase.pos.y += 12;
+                if (storeCase.pos.y + storeCase.height > H && !storeCase.drops) {
+                    addDrops(storeCase.pos.x);
+                    storeCase.drops = true;
+                }
+                ctx.stroke();
+
+            }
+
+            for (let i = 0, l = dropsStore.length; i < l; i++) {
+                const drop: dropsType = dropsStore[i];
+                if (!drop) continue;
+                ctx.beginPath();
+
+                if (drop.pos.y > H) {
+                    dropsStore.splice(i, 1);
+                    continue;
+                }
+                ctx.fillStyle = "#6EACEC";
+                ctx.arc(drop.pos.x, drop.pos.y, drop.radius, 0, 2 * Math.PI);
+                ctx.fill();
+                drop.pos.x += drop.speed;
+                drop.pos.y = drop.fn(drop.pos.x);
+
+            }
+            if (timeStamp - startTime > 200) {
+                store.push(
+                    {
+                        pos: {
+                            x: Math.random()*W,
+                            y: 0
+                        },//坐标
+                        height: 50*Math.random()+20,//长度
+                        drops: false
+                    },
+                )
+                startTime = timeStamp;
+            }
+            // console.log(timeStamp);
+
+            window.requestAnimationFrame(render);
+        };
+        render();
+
     }
-    console.log(dropsStore)
 
-};
-
-
-let func;
-//
-const render = (): void => {
-    // console.log('run');
-    ctx.fillStyle = "#000";
-    ctx.fillRect(0, 0, W, H);
-
-    // if (!func) {
-    //     func = getParabolaFunc({px: 200, py: 200}, {x, y});
-    // }
-    //
-    // ctx.lineTo(x += 1, func(x));
-    //
-    // console.log(x, func(x));
-    //
-
-    for (let i = 0, l = store.length; i < l; i++) {
-        ctx.beginPath();
-        const storeCase: rainType = store[i];
-        if (storeCase.pos.y > H) {
-            store.splice(i, 1);
-            continue;
-        }
-        ctx.moveTo(storeCase.pos.x, storeCase.pos.y);
-        ctx.lineTo(storeCase.pos.x, storeCase.pos.y + storeCase.height);
-        storeCase.pos.y += 12;
-        if (storeCase.pos.y + storeCase.height > H && !storeCase.drops) {
-            console.log('drops')
-            addDrops(storeCase.pos.x);
-            storeCase.drops = true;
-        }
-
-    }
-    ctx.stroke();
-
-    for(let i=0,l=dropsStore.length;i<l;i++){
-        ctx.beginPath();
-
-        const drop = dropsStore[i];
-        console.log(drop);
-        // if(drop.pos.y>H){
-        //     delete dropsStore[i];
-        //     continue;
-        // }
-        ctx.fillStyle = "#fff";
-        ctx.arc(drop.pos.x,drop.pos.y,2,0,2*Math.PI);
-        drop.pos.x+=drop.speed;
-        drop.pos.y = drop.fn(drop.pos.x);
-        // if(drop.pos.y>H){
-        //     delete dropsStore[i];
-        //     continue;
-        // }
-    }
-    ctx.fill();
-
-
-
-
-    // if (x > 400) {
-    //     return;
-    // }
-
-    window.requestAnimationFrame(render);
-};
-render();
+)()
