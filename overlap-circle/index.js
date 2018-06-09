a883bef148b42cfa8244e74a0d0b2afe1b83c5ab
zhangyu <grewer@grewer.cn>
2018-06-09 23:01:44 +0800

    refactor(module) : 变化需求,可以部分重叠,效果参考 game center 图标
    TODO : 宽度变化待定

---
+++ overlap-circle/index.js
@@ -0,0 +1,72 @@
+let obj = document.getElementById('print');
+const width = obj.clientWidth
+const height = obj.clientHeight
+
+console.log(width, height)
+
+const data = [
+  {
+    type: '语文',
+    score: 50
+  },
+  {
+    type: '数学',
+    score: 80
+  },
+  {
+    type: '英语',
+    score: 20
+  },
+  {
+    type: '物理',
+    score: 77
+  },
+  {
+    type: '化学',
+    score: 66
+  },
+  {
+    type: '生物',
+    score: 33
+  }
+]
+// 入口数据
+
+
+// 对入口数据做处理
+const countScore = data.reduce((t, o) => {
+  return t + o.score
+}, 0)
+
+const store = data.map(i => {
+  i.percent = ((+i.score) / countScore).toFixed(2)
+  return i
+})
+
+const minL = width > height ? height : width
+
+
+for (let i = 0, l = store.length; i < l; i++) {
+
+  let radius = minL * (+store[i].percent) / 2
+  let pX = Math.floor(Math.random() * (width - radius))
+  let pY = Math.floor(Math.random() * (height - radius))
+
+  showResult(i,radius,radius,pX,pY)
+}
+
+function showResult(i, elementSize, elementRadius, elementLeft, elementTop) {
+
+  let treeElement = document.createElement("div");
+
+  treeElement.setAttribute("id", "tree" + i);
+  treeElement.style.width = elementSize + "px";
+  treeElement.style.height = elementSize + "px";
+  treeElement.style.borderRadius = elementRadius + "px";
+  treeElement.style.left = elementLeft + "px";
+  treeElement.style.top = elementTop + "px";
+  treeElement.style.backgroundColor = "rgba(" + Math.floor(Math.random() * 256) + "," + Math.floor(Math.random() * 256) + "," + Math.floor(Math.random() * 256) + "," + "0.9)";
+
+  obj.appendChild(treeElement);
+}
+
